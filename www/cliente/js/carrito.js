84b1fdd2131b978e348ffb24d78f98ae52caae96
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2024-04-15 19:00:46 +0200

    minor updates

--- www/cliente/js/carrito.js
+++ www/cliente/js/carrito.js
@@ -178,11 +178,11 @@ var nav = document.querySelector("#nav1");
 var abrir = document.querySelector("#abrir");
 var cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("touch", () => {
+abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 });
 
-cerrar.addEventListener("touch", () => {
+cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 });
 
